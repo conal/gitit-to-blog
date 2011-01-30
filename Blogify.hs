0544ca2baec8764500a0dfbb794667c44fc63dac
Conal Elliott <conal@conal.net>
2011-01-30 10:57:44 -0800

    now removes <!-- references -->, Introduction section, and <!--[ ... ]-->

--- Blogify.hs
+++ Blogify.hs
@@ -19,7 +19,7 @@
 
 module Main (main) where
 
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf,isSuffixOf)
 
 import Text.Pandoc
 
@@ -28,13 +28,22 @@ import qualified Network.Gitit.Plugin.FixSymbols as Sym
 type Unop a = a -> a
 
 -- * Remove gitit page meta-data if present (before parsing)
+-- * Remove "Introduction" section header
+-- * Remove "<!-- references -->"
 -- * Deepen all headers by two levels
+-- * Remove comments surrounded by <!--[ ... ]-->
 -- * Fix source links: "src/" --> "/blog/src/"
 -- * Rewrite some code symbols
 
 tweakBlock :: Unop Block
+tweakBlock (Plain [HtmlInline "<!-- references -->"]) = Null
+tweakBlock (Header 1 [Str "Introduction"]) = Null
 tweakBlock (Header n xs) = Header (n+2) xs
+tweakBlock (Plain [HtmlInline s]) | isPrefixOf "<!--[" s && isSuffixOf "]-->" s = Null
 tweakBlock x = Sym.fixBlock x
+
+-- TODO: handle comments via my gitit-comments plugin. Awkward now with
+-- the types used there (Block -> [Block] and Inline -> [Inline]).
 
 -- Link [Str foo] ("src/xxx",title) --> Link [Str foo] ("blog/src/xxx",title)
 
@@ -67,5 +76,8 @@ writeDoc :: Pandoc -> String
 -- writeDoc = writeMarkdown defaultWriterOptions
 writeDoc = writeHtmlString defaultWriterOptions
 
+rewrite :: Unop String
+rewrite = writeDoc . transformDoc . readDoc . onLines dropMeta
+
 main :: IO ()
-main = interact (writeDoc . transformDoc . readDoc . onLines dropMeta)
+main = interact rewrite
