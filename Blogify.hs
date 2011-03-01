89740fb58cbff0644d00e9ae4cc85a7da1401272
Conal Elliott <conal@conal.net>
2011-03-01 10:00:38 -0800

    use Comment plugin; fix handling of <!--references-->

--- Blogify.hs
+++ Blogify.hs
@@ -19,6 +19,7 @@
 
 module Main (main) where
 
+import Data.Maybe (fromMaybe)
 import Data.List (isPrefixOf,isSuffixOf,isInfixOf)
 
 import Text.Pandoc
@@ -27,35 +28,37 @@ import Paths_gitit_to_blog (getDataFileName)
 
 import qualified Network.Gitit.Plugin.FixSymbols     as Sym
 import qualified Network.Gitit.Plugin.BirdtrackShift as Bird
+import qualified Network.Gitit.Plugin.Comment        as Com
 
-type Unop a = a -> a
-
+-- Steps:
+-- 
 -- * Indent lines starting with "<" unless followed by space.
 --   Distinguishes inverse bird tracks from HTML.
 -- * Remove gitit page meta-data if present (before parsing)
 -- * Remove "Introduction" section header
--- * Remove "<!-- references -->"
 -- * Deepen all headers by two levels
 -- * Remove comments surrounded by <!--[ ... ]-->
 -- * Fix source links: "src/" --> "/blog/src/"
 -- * Rewrite some code symbols
+-- * "<!-- references -->" and adjacent newlines from start of doc.
+
+
+type Unop a = a -> a
 
 tweakBlock :: Unop Block
 tweakBlock (RawBlock "html" "<!-- references -->") = Null
 tweakBlock (Header 1 [Str "Introduction"]) = Null
 tweakBlock (Header n xs) = Header (n+2) xs
 tweakBlock (RawBlock "html" s) | isPrefixOf "<!--[" s && isSuffixOf "]-->" s = Null
-tweakBlock x = Sym.fixBlock x
+tweakBlock x = (Com.fixBlock . Sym.fixBlock) x
 
--- TODO: handle comments via my gitit-comments plugin. Awkward now with
--- the types used there (Block -> [Block] and Inline -> [Inline]).
 
 -- Link [Str foo] ("src/xxx",title) --> Link [Str foo] ("blog/src/xxx",title)
 
 tweakInline :: Unop Inline
 tweakInline (Link inlines (url,title)) | isPrefixOf "src/" url =
   Link inlines ("/blog/" ++ url,title)
-tweakInline x = Sym.fixInline x
+tweakInline x = (Com.fixInline . Sym.fixInline) x
 
 transformDoc :: Unop Pandoc
 transformDoc = bottomUp tweakInline . bottomUp tweakBlock
@@ -75,6 +78,27 @@ dropMeta :: Unop [String]
 dropMeta ("---":rest) = tail $ dropWhile (/= "...") rest
 dropMeta ss = ss
 
+-- Drop given prefix or yield original if no match.
+dropPrefix :: Eq a => [a] -> [a] -> [a]
+dropPrefix p s = fromMaybe s (dropPrefix' p s)
+
+-- Drop prefix and yield suffix, if prefix matched
+dropPrefix' :: Eq a => [a] -> [a] -> Maybe [a]
+dropPrefix' [] bs                     = Just bs
+dropPrefix' _  []                     = Nothing
+dropPrefix' (a:as) (b:bs) | a == b    = dropPrefix' as bs
+                          | otherwise = Nothing
+
+trimNewlines :: Unop String
+trimNewlines (c:cs) | c `elem` nlChars = trimNewlines cs
+ where nlChars = "\n\r"
+trimNewlines s                       = s
+
+trimBlankRefs :: Unop String
+trimBlankRefs = trimNewlines
+              . dropPrefix "<!-- references -->"
+              . trimNewlines  
+
 readDoc :: String -> Pandoc
 readDoc = readMarkdown (defaultParserState {stateLiterateHaskell = True})
 
@@ -85,6 +109,7 @@ writeDoc = writeHtmlString (defaultWriterOptions { writerHTMLMathMethod = MathML
 
 rewrite :: String -> Unop String
 rewrite mathJS = mbMath mathJS
+               . trimBlankRefs
                . writeDoc
                . transformDoc
                . readDoc
@@ -95,8 +120,10 @@ mbMath :: String -> Unop String
 mbMath js html | isInfixOf "<math " html = html ++ wrapJS js
                | otherwise               = html
 
+-- <script language="javascript" type="text/javascript">
+
 wrapJS :: Unop String
-wrapJS = ("<script type=\"text/javascript\">" ++) . (++ "</script>")
+wrapJS = ("<script language=\"javascript\" type=\"text/javascript\">" ++) . (++ "</script>")
 
 main :: IO ()
 main = getDataFileName "data/MathMLinHTML.js" >>= readFile >>= interact . rewrite
