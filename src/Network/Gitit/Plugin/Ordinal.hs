d9fe718892397e3536746cf9c98575e17fc76c04
Conal Elliott <conal@conal.net>
2016-09-19 08:59:49 -0700

    punctuation after ordinal

--- src/Network/Gitit/Plugin/Ordinal.hs
+++ src/Network/Gitit/Plugin/Ordinal.hs
@@ -14,22 +14,30 @@
 
 module Network.Gitit.Plugin.Ordinal (plugin, fixInline) where
 
+import Control.Monad (guard)
+import Data.Maybe (listToMaybe)
+import Data.Char (isPunctuation)
+
 import Network.Gitit.Interface
 
 plugin :: Plugin
 plugin = PageTransform $ return . bottomUp (concatMap fixInline)
 
 fixInline :: Inline -> [Inline]
-fixInline (Str s) | Just (n,suff) <- splitOrdinal s =
-  [Str (show n), Superscript [Str suff]]
+fixInline (Str s) | Just (n,suff,punct) <- splitOrdinal s =
+  [Str (show n), Superscript [Str suff], Str punct]
 fixInline x = [x]
 
 -- | Split an ordinal string in prefix & suffix. For instance, "21st" ->
 -- Just ("21","st"). Note: allows questionable ordinals like "23th".
 
-splitOrdinal :: String -> Maybe (Int,String)
-splitOrdinal s | [(n,suff)] <- reads s, isOrdinalSuffix suff = Just (n,suff)
-splitOrdinal _ = Nothing
+splitOrdinal :: String -> Maybe (Int,String,String)
+-- splitOrdinal s | [(n,suff)] <- reads s, isOrdinalSuffix suff = Just (n,suff,"")
+-- splitOrdinal _ = Nothing
+splitOrdinal s = do (n,suff1) <- listToMaybe (reads s)
+                    let (osuff, punct) = splitAt 2 suff1
+                    guard (isOrdinalSuffix osuff && all isPunctuation punct)
+                    return (n,osuff,punct)
 
 isOrdinalSuffix :: String -> Bool
 isOrdinalSuffix = (`elem` ["st", "nd", "rd", "th"])
