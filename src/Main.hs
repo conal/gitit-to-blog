f6786af35c864ff7910be8fba377bb84c50f4b3b
Conal Elliott <conal@conal.net>
2019-10-17 19:14:21 -0700

    drop use of MathJax

--- src/Main.hs
+++ src/Main.hs
@@ -256,11 +256,11 @@ writeDoc private =
 
 -- Without writerTemplate, I lose all of my output when writerStandalone = True.
 wTemplate :: Bool -> String
-wTemplate private = unlines
+wTemplate _private = unlines
   [ 
     "<title>$title$</title>"
   , "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>"
-  , "<script src=\"" ++ mathJaxUrl ++ "/MathJax.js?config=TeX-AMS-MML_HTMLorMML\" type=\"text/javascript\"></script>"
+  -- , "<script src=\"" ++ mathJaxUrl ++ "/MathJax.js?config=TeX-AMS-MML_HTMLorMML\" type=\"text/javascript\"></script>"
   , "<link rel=\"stylesheet\" href=\"file:///Users/conal/Journals/Current/wikidata/static/css/custom.css\" media=\"all\" type=\"text/css\"/>"
   , "<style>blockquote { padding-top: 0em; }</style>"
   -- , "<style media=print>body { font-size:100%; }</style>"
@@ -269,9 +269,9 @@ wTemplate private = unlines
   , "$body$"
   , "</body>"
   ]
- where
-   mathJaxUrl | private   = "https://cdn.mathjax.org/mathjax/latest"
-              | otherwise = "file:///Users/conal/Downloads/MathJax-master"
+ -- where
+ --   mathJaxUrl | private   = "https://cdn.mathjax.org/mathjax/latest"
+ --              | otherwise = "file:///Users/conal/Downloads/MathJax-master"
 
 -- There is another critically important step, which is to include the
 -- contents of data/MathMLinHTML.js from Pandoc in my blog.
